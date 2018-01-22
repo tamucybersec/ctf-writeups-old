e66ac5811f1577dfdc04bbd2efaf64c4d8ab4971
Administrator <admin@example.com>
2018-01-22 12:05:11 -0600

    fix monoshift

--- hackerfire/MonoShift-Messy/monoshift_solve.py
+++ hackerfire/MonoShift-Messy/monoshift_solve.py
@@ -7,9 +7,9 @@ for i in range(26):
     plain = ""
     for char in ciphertext:
         if char.isalpha():
-            num = ord(char) + i
-            if num > ord('z'):
-                num -= 26
+            num = ord(char) - i
+            if num < ord('a'):
+                num += 26
             plain += chr(num)
         else:
             plain += char
